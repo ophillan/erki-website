bd78d5f9e45ba320fc0bc9d7601243a4179b4faf
Mart M <repogfreow@gmail.com>
2016-06-04 17:57:15 +0300

    updating info page

--- resources/lang/ee/info.php
+++ resources/lang/ee/info.php
@@ -2,21 +2,36 @@
 
 return [
     'whatcauses' => 'Mis põhjustab osteoartriiti?',
-    'whatcausestext1' => 'Primaarne osteoartriit on peamiselt seotud vananemisega. Vananemisel kõhre veesisaldus suureneb ja kõhr hakkab taandarenema. Kaugele arenenud juhtudel kaob kõhr liigestes asuvate luude vahelt täielikult. Kulunud liigeste korduv kasutamine võib kõhre ärritada ja põletikku tekitada, põhjustades valu ja turset. Kõhre kadumise tagajärjel toimub liigestes asuvate luude vahel suurem hõõrdumine. See põhjustab valu ja liigeste liikuvuse vähenemist. Kõhre põletik võib soodustada uue luukoe kasvu, nagu liigeste ümber moodustuvad ogad.',
-    'whatcausestext2' => 'Sekundaarset osteoartriiti põhjustavad sellised seisundid nagu ülekaalulisus, korduv trauma või liigese lõikus, sünnipärased ebanormaalsed liigesed (kaasasündinud väärarengud), diabeet ja teised hormonaalsed häired.',
+    'whatcausestext1' => 'Osteoartriit on kõige sagedamini esinev liigest kahjustav haigus. Osteoartriiti iseloomustab liigesekõhre degeneratsioon ehk hävinemine. Selle struktuuri destruktsiooni tõttu tekib luude omavaheline hõõrdumine, mis põhjustab valu, jäikust ning väheneb liigesliikuvus. OA ei kahjusta ainult kõhre, vaid haarab kogu liigest tervikuna. Haiguse puhul esinevad põletikulised episoodid.',
+    'whatcausestext2' => 'Haiguse algus on tavaliselt aeglane ning seetõttu märkamatu. Enamasti tekib valu liikumisel, millega kaasneb krigin, kuid rahuolekus valu kaob. Iseloomult on valu tuim ning tugevneb päeva teises pooles, hommikuti on tunne parem.  Haiguse progresseerumisel on valud püsivamad ning tekivad luulised vohandid.',
+    'whatcausestext3' => 'Episooditi võivad haigetes liigestes tekkida põletikunähud, mille tagajärjel toimub veel intensiivsem kõhre hävinemine. Sel puhul aga ei vähene valu  ka rahuolekus, tekib öine valu ning hommikuti esinev liigesjäikus tugevneb. Liigeses esineb turse, on katsudes valulik ning soe, liikuvus vähenenud. ',
+
 
     'symptoms' => 'Sümptomid',
-    '' => '',
-    '' => '',
-    '' => '',
-
+    'symptoms1' => 'Valu koormusel või liikumisel, mis väheneb puhkusega',
+    'symptoms2' => 'Liikumisel naksumine, plõksumine, krigin',
+    'symptoms3' => 'Punetus, turse',
+    'symptoms4' => 'Hommikune liigesjäikus kuni 30 min',
+    'symptoms5' => 'Esinevad nn stardivalud',
 
     'riskfactors' => 'Riskifaktorid',
-    '' => '',
-    '' => '',
+    'riskfactor1' => 'Vanus (haigestumine on sagedasem vanematel inimestel)',
+    'riskfactor2' => 'Ülekoormus liigesele (ülekaalulisus, raske füüsiline töö, töö püstasendis, tippsport)',
+    'riskfactor3' => 'Mikrotraumad ja põrutused',
+    'riskfactor4' => 'Rühihäired (düsplaasia, genu varu, genu valgum, skolioos, küfoos, hüperlordoos, lamppöidsus, hüpermobiilsus)',
+    'riskfactor5' => 'Ainevahetus- või endokriinsed häired (diabeet, akromegaalia, menopaus)',
+    'riskfactor6' => 'Perifeersed vereringehäired',
+    'riskfactor7' => 'Pärilikkus',
+    'riskfactor8' => 'C- ja D-vitamiinivähesus',
 
 
     'treatment' => 'Ravivõimalused',
-    '' => '',
-    '' => '',
+    'treatmenttext' => 'Ravi eesmärgiks on valu ning põletiku vähendamine, liigesfunktsiooni taastamine või säilitamine, operatsiooni vältimine või edasilükkamine.',
+    'treatmentoptions' => 'Raviks on erinevaid võimalusi.',
+    'treatmentoption1' => 'Operatsioon',
+    'treatmentoption1_text' => '- kallis, kuid ravib probleemi pikemaks ajaks.',
+    'treatmentoption2' => 'Sünoviaal- ehk liidesevedeliku süst',
+    'treatmentoption2_text' => '- taskukohane ning süsti mõju kestab kuus kuud, misjärel võib probleemide tekkimise korral uuesti arsti poole pöörduda.',
+    'treatmentoption3' => 'Füsioteraapia',
+    'treatmentoption3_text' => '- taskukohane ning aitab sümptomeid leevendada.',
 ];
--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -11,25 +11,39 @@
             <hr>
 
             <h2 class="paragraph-heading" id="symptoms">{{ trans('info.symptoms') }}</h2>
-            <p class="paragraph-text">{{ trans('info.symptomstext') }}</p>
             <ul>
-                <li>{{ trans('info.symptoms1') }}</li>
-                <li>{{ trans('info.symptoms2') }}</li>
-                <li>{{ trans('info.symptoms3') }}</li>
-                <li>{{ trans('info.symptoms4') }}</li>
+                <li class="paragraph-text">{{ trans('info.symptoms1') }}</li>
+                <li class="paragraph-text">{{ trans('info.symptoms2') }}</li>
+                <li class="paragraph-text">{{ trans('info.symptoms3') }}</li>
+                <li class="paragraph-text">{{ trans('info.symptoms4') }}</li>
+                <li class="paragraph-text">{{ trans('info.symptoms5') }}</li>
             </ul>
 
             <hr>
 
             <h2 class="paragraph-heading" id="riskfactors">{{ trans('info.riskfactors') }}</h2>
-            <p class="paragraph-text">{{ trans('info.riskfactortext1') }}</p>
-            <p class="paragraph-text">{{ trans('info.riskfactortext2') }}</p>
+            <ul>
+                <li class="paragraph-text">{{ trans('info.riskfactor1') }}</li>
+                <li class="paragraph-text">{{ trans('info.riskfactor2') }}</li>
+                <li class="paragraph-text">{{ trans('info.riskfactor3') }}</li>
+                <li class="paragraph-text">{{ trans('info.riskfactor4') }}</li>
+                <li class="paragraph-text">{{ trans('info.riskfactor5') }}</li>
+                <li class="paragraph-text">{{ trans('info.riskfactor6') }}</li>
+                <li class="paragraph-text">{{ trans('info.riskfactor7') }}</li>
+                <li class="paragraph-text">{{ trans('info.riskfactor8') }}</li>
+            </ul>
 
             <hr>
 
             <h2 class="paragraph-heading" id="treatment">{{ trans('info.treatment') }}</h2>
-            <p class="paragraph-text">{{ trans('info.treatmenttext1') }}</p>
-            <p class="paragraph-text">{{ trans('info.treatmenttext2') }}</p>
+            <p class="paragraph-text">{{ trans('info.treatmenttext') }}</p>
+            <p class="paragraph-text">{{ trans('info.treatmentoptions') }}</p>
+
+            <ul>
+                <li class="paragraph-text"><span class="paragraph-heading" style="font-size: 14px;">{{ trans('info.treatmentoption1') }}</span> {{trans('info.treatmentoption1_text')}}</li>
+                <li class="paragraph-text"><span class="paragraph-heading" style="font-size: 14px;">{{ trans('info.treatmentoption2') }}</span> {{trans('info.treatmentoption2_text')}}</li>
+                <li class="paragraph-text"><span class="paragraph-heading" style="font-size: 14px;">{{ trans('info.treatmentoption3') }}</span> {{trans('info.treatmentoption3_text')}}</li>
+            </ul>
 
         </div>
     </div>
