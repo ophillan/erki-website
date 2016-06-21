8d5ccd40e0a732d703094a6a63d90e93efa7039e
Mart M <repogfreow@gmail.com>
2016-06-21 12:56:59 +0300

    bootstrap

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -3,34 +3,38 @@
 @section('content')
 
     <!-- First block -->
-    <div class="row row1">
-        <div class="col-md-6">
-            <h2 class="paragraph-heading">
-                {{ trans('landing.whatis') }}
-            </h2>
-            <p class="paragraph-text">{{ trans('landing.whatistext') }}</p>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2 col-sm-12 col-xs-12">
+            <div class="col-md-6">
+                <h2 class="paragraph-heading">
+                    {{ trans('landing.whatis') }}
+                </h2>
+                <p class="paragraph-text">{{ trans('landing.whatistext') }}</p>
 
-            <h2 class="paragraph-heading">
-                {{ trans('landing.symptoms') }}
-            </h2>
-            <p class="paragraph-text">{{ trans('landing.symptomstext') }}</p>
-        </div>
+                <h2 class="paragraph-heading">
+                    {{ trans('landing.symptoms') }}
+                </h2>
+                <p class="paragraph-text">{{ trans('landing.symptomstext') }}</p>
+            </div>
 
-        <div class="col-md-6">
-            <img src="/img/landing/oa_knee.png" alt="Osteoarthritis" class="img-responsive paragraph-image">
+            <div class="col-md-6">
+                <img src="/img/landing/oa_knee.png" alt="Osteoarthritis" class="img-responsive paragraph-image">
+            </div>
         </div>
     </div>
 
     <!-- Second block -->
-    <div class="row row2">
-        <div class="col-md-6">
-            <img src="/img/landing/viscoplus.png" alt="Viscoplus" class="img-responsive paragraph-image">
-        </div>
-        <div class="col-md-6">
-            <h2 class="paragraph-heading">
-                {{ trans('landing.whatweoffer') }}
-            </h2>
-            <p class="paragraph-text">{{ trans('landing.whatweoffertext') }}</p>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2 col-sm-12 col-xs-12">
+            <div class="col-md-6">
+                <img src="/img/landing/viscoplus.png" alt="Viscoplus" class="img-responsive paragraph-image">
+            </div>
+            <div class="col-md-6">
+                <h2 class="paragraph-heading">
+                    {{ trans('landing.whatweoffer') }}
+                </h2>
+                <p class="paragraph-text">{{ trans('landing.whatweoffertext') }}</p>
+            </div>
         </div>
     </div>
 @stop
