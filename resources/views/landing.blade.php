d3e1d08549f962367c9809acbf7af0b6bfbffa8e
Mart M <repogfreow@gmail.com>
2016-06-23 13:14:24 +0300

    bootstrap col fix

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -4,8 +4,8 @@
 
     <!-- First block -->
     <div class="row" id="contentrow">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="col-md-6">
+        <div class="col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+            <div class="col-sm-6">
                 <h2>{{ trans('landing.whatis') }}</h2>
                 <p>{{ trans('landing.whatistext') }}</p>
 
@@ -13,15 +13,15 @@
                 <p>{{ trans('landing.symptomstext') }}</p>
             </div>
 
-            <div class="col-md-6"><img id="picture1" src="/img/landing/osteo.png" alt="Osteoarthritis" class="img-responsive"></div>
+            <div class="col-sm-6"><img id="picture1" src="/img/landing/osteo.png" alt="Osteoarthritis" class="img-responsive"></div>
         </div>
     </div>
 
     <!-- Second block -->
     <div class="row" id="contentrow">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="col-md-6"><img src="/img/landing/viscoplus.png" alt="ViscoPlus" class="img-responsive"></div>
-            <div class="col-md-6">
+            <div class="col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+            <div class="col-sm-6"><img src="/img/landing/viscoplus.png" alt="ViscoPlus" class="img-responsive"></div>
+            <div class="col-sm-6">
                 <h2>{{ trans('landing.whatweoffer') }}</h2>
                 <p>{{ trans('landing.whatweoffertext') }}</p>
             </div>
