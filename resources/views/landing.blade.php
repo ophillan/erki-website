5976f4603d0750f5dd2e0812544de5bbe1ec1854
Mart M <repogfreow@gmail.com>
2016-06-24 13:13:07 +0300

    picture swap done

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -13,13 +13,22 @@
                 <p>{{ trans('landing.symptomstext') }}</p>
             </div>
 
-            <div class="col-sm-6"><img id="picture1" src="/img/landing/osteo.png" alt="Osteoarthritis" class="img-responsive"></div>
+            <div class="col-sm-6">
+                <picture>
+                    <source media="(max-width: 768px)" srcset="/img/landing/osteo.png">
+                    <source media="(min-width: 769px) and (max-width: 1400px)" srcset="/img/landing/osteo-small.png">
+                    <source media="(min-width: 1201px)" srcset="/img/landing/osteo.png">
+                    <img id="picture1" class="img-responsive" src="/img/landing/osteo.png" alt="Space Needle">
+                </picture>
+            </div>
+
+
         </div>
     </div>
 
     <!-- Second block -->
     <div class="row" id="contentrow">
-            <div class="col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+        <div class="col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
             <div class="col-sm-6"><img src="/img/landing/viscoplus.png" alt="ViscoPlus" class="img-responsive"></div>
             <div class="col-sm-6">
                 <h2>{{ trans('landing.whatweoffer') }}</h2>
